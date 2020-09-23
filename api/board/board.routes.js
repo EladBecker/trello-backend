b31238246767098656e37922514b861e49460b31
EladBecker <becker.elad@gmail.com>
2020-09-24 01:15:36 +0300

    added add and removing board routes

--- api/board/board.routes.js
+++ api/board/board.routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { requireAuth, requireAdmin } = require('../../middlewares/requireAuth.middleware')
-const { getBoards, getBoard, updateBoard, deleteBoard  } = require('./board.controller')
+const { getBoards, getBoard, updateBoard, deleteBoard, addBoard } = require('./board.controller')
 const router = express.Router()
 
 // middleware that is specific to this router
@@ -8,9 +8,10 @@ const router = express.Router()
 
 router.get('/', getBoards)
 router.get('/:id', getBoard)
+router.post('/', addBoard)
 // router.put('/:id', requireAuth, updateBoard)
 router.put('/:id', updateBoard)
-router.delete('/:id', requireAuth, deleteBoard)
+// router.delete('/:id', requireAuth, deleteBoard)
 router.delete('/:id', deleteBoard)
 
 module.exports = router
