3df301148f31e3eec28ce544c3e35b3152ae5295
xXDaniel123 <daniel91dante@gmail.com>
2020-09-29 00:19:34 +0300

    login

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -5,7 +5,7 @@ async function getUser(req, res) {
     const user = await userService.getById(req.params.id)
     res.send(user)
 }
-  
+
 async function getUsers(req, res) {
     const users = await userService.query(req.query)
     res.send(users)
@@ -22,9 +22,20 @@ async function updateUser(req, res) {
     res.send(user)
 }
 
+//
+
+async function login(req, res){
+    console.log(req);
+    
+}
+
+
+//
+
 module.exports = {
     getUser,
     getUsers,
     deleteUser,
-    updateUser
+    updateUser,
+    login
 }
